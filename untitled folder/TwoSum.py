88667b71392f71e5fc321f4555de3bfcf397c8cf
thebenfirsty <149822194+thebenfirsty@users.noreply.github.com>
2024-04-24 10:33:09 -0400

    Initial Commit

---
+++ untitled folder/TwoSum.py
@@ -0,0 +1,52 @@
+#My Solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        size = len(nums)
+        for i in range(size):
+            test = nums[i]
+            for j in range(size):
+                if( i == j):
+                    pass
+                elif((nums[i] + nums[j]) == target):
+                    return [i,j]
+
+
+#Ideal solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        return []
+
+
+# Problem Definition
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+#
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+#
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
